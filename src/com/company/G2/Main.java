f021337768058d042e330d931c53521751634d4f
Mohamed Taha AbdElAziz <1212.mohamed.taha@gmail.com>
2021-06-09 22:59:13 +0200

    first commit

---
+++ src/com/company/G2/Main.java
@@ -0,0 +1,30 @@
+package com.company.G2;
+
+import java.awt.*;
+import java.io.File;
+import java.util.List;
+
+public class Main {
+    static String Path = "C:\\Users\\Mohamed Taha\\Desktop\\Java\\Java and UML_Amr Elshafey\\pyramids.csv"; // This is the path ?
+
+    public static void main(String[] args) {
+        PyramidCSVDAO pDAO = new PyramidCSVDAO(Path);
+
+        List<Pyramid> pyramids = pDAO.readPyramidFromCSV();
+
+        //int i = 0;
+        for(Pyramid p:pyramids) {
+            System.out.println("#" +  p );
+        }
+
+
+
+        /**File file = new File("C:\\Users\\Mohamed Taha\\Desktop\\Java\\Java and UML_Amr Elshafey\\");
+        System.out.println("This is the Start:");
+        String[] fileList = file.list();
+        for (String name:fileList){
+            System.out.println(name);
+        }
+        System.out.println("This is the End:");**/
+    }
+}
---
+++ src/com/company/G2/PyramidCSVDAO.java
@@ -0,0 +1,45 @@
+package com.company.G2;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PyramidCSVDAO {
+    List<Pyramid>  pyramidInfo  ;  // initializing the list
+
+    String Path ;     // This is the path ?
+
+
+
+    public PyramidCSVDAO(String fileName)  {   // Constructor that an object will take a string carrying the file path
+        this.Path = fileName;
+        this.pyramidInfo = new ArrayList<Pyramid>();
+
+    }
+
+    public List<Pyramid> readPyramidFromCSV(){    // The Method that will read and return a list of pyrmids
+        File pyramidFile = new File(Path);     // Creating a file object
+        List<String> lines = null;  // initializing the list of strings that will carry the lines
+
+
+        try {
+            lines = Files.readAllLines(pyramidFile.toPath());  // Trying to read the file and store it at lines ,, I don't know what toPath() does.
+        } catch (Exception e){
+            System.out.println("error during reading the file");}
+
+        for (int L =1; L < lines.size(); L ++){   // Reading Line by Line
+            String line = lines.get(L);
+
+            String[] fields = line.split(",");
+
+            for (int field = 0; field< fields.length; field ++){  //Reading field by field
+                fields[field] = fields[field].trim();
+            }
+            Pyramid pyramid = new Pyramid(fields[2],fields[0],fields[4],fields[7]);
+            this.pyramidInfo.add(pyramid);
+        }
+        return pyramidInfo;
+        }
+
+}
